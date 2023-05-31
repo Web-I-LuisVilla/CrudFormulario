5e2fd810363e843157c0f3a2762f46ff8ac53974
luis eduardo villa ojeda <112597598+lvilla8@users.noreply.github.com>
2023-05-31 12:05:28 -0500

    f

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import CrudApp from "./Components/CrudApp";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>CrudUser</h1>
+      <CrudApp />
+    </>
+  );
+}
+
+export default App;
